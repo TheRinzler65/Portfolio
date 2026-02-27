2641bdcac11ed9284ceaf14cf4fa0b5fb6a5c956
TheRinzler65 <leo.gruet65@gmail.com>
2026-02-27 11:50:46 +0100

    début de modification des infos

--- src/data/miscProjects.ts
+++ src/data/miscProjects.ts
@@ -1,29 +1,29 @@
 export const miscProjects = [
   {
     id: 1,
-    title: "Script d'automatisation Backup",
+    title: "Création d'une infrastructure réseau avec Proxmox Virtual Environment",
     description:
-      "Script Bash pour automatiser la sauvegarde des serveurs Linux vers un NAS Synology.",
-    tags: ["Bash", "Linux", "Automation"],
-    repoLink: "https://github.com/...",
+      "",
+    tags: ["Proxmox", "Infra", "Serveurs", "Virtualisation"],
+    repoLink: "",
     demoLink: null,
   },
   {
     id: 2,
-    title: "Config Home Assistant",
+    title: "Création d'un site web pour la traduction d'un jeu vidéo",
     description:
-      "Ma configuration complète pour la domotique (Lumières, Chauffage, Monitoring Réseau).",
-    tags: ["YAML", "IoT", "Docker"],
-    repoLink: "https://github.com/...",
-    demoLink: "#",
+      "",
+    tags: ["Patch", "Traduction", "Nintendo 3DS", "Web"],
+    repoLink: "",
+    demoLink: null,
   },
   {
     id: 3,
-    title: "Bot Discord Modération",
+    title: "",
     description:
-      "Bot codé en Python pour gérer la modération automatique d'un serveur communautaire.",
-    tags: ["Python", "Discord.py", "API"],
-    repoLink: "https://github.com/...",
+      "",
+    tags: ["", "", ""],
+    repoLink: "",
     demoLink: null,
   },
 ];
